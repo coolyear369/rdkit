a634b609fab1b27ec70ac4238662a8c8ecd6f229
coolyear <coolyear369@gmail.com>
2024-08-19 20:24:05 +0800

    cool

--- practice.py
+++ practice.py
@@ -3,11 +3,24 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import seaborn as sns
+from rdkit import Chem
+import requests
+
+morphine_url = "https://go.drugbank.com/structures/small_molecule_drugs/DB00295.mol"
+morphine_mol = requests.get(morphine_url).text
+morphine = Chem.MolFromMolBlock(morphine_mol)
+
+from rdkit.Chem import Draw
+from rdkit.Chem import AllChem
+
+img2 = Draw.MolToImage(morphine)
+print(morphine.GetNumAtoms())
+H = Chem.AddHs(morphine)
+AllChem.Compute2DCoords(H)
+Hp = Draw.MolToImage(H)
+Hp.show()
 
 
-df = pd.read_csv("cardata.csv")
-x = df.iloc[:, :3]
-y = df.iloc[:, 3]
 
-print(x)
-print(y)
+
+
